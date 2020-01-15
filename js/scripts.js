074edc1d0a7aefe9ebe71e4be345b2b6516ddc72
Maratib Ali Khan <maratib@gmail.com>
2020-01-15 21:18:43 +0500

    Initial Commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,20 @@
-// $(document).ready(function () {
-// });
+$(document).ready(function () {
+	$(".navbar-toggler").click(function () {
+		$(".navbar-toggler").toggleClass(" change ");
+	});
+
+});
+
+
+$(window).scroll(function () {
+	let position = $(this).scrollTop();
+	if (position >= 150) {
+		$("nav")
+			.addClass("custom-menu")
+			.removeClass("nav-menu");
+	} else {
+		$("nav")
+			.addClass("nav-menu")
+			.removeClass("custom-menu");
+	}
+});
